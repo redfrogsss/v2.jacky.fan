adbb4669e4961edea506471389daebdebe9af99d
Jacky Fan <contact@jacky.fan>
2023-08-21 22:12:23 +0800

    chore: setup daisyui theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
     plugins: [require("daisyui")],
     // daisyUI config (optional - here are the default values)
     daisyui: {
-        themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+        themes: ["winter", "night"], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
         darkTheme: "night", // name of one of the included themes for dark mode
         base: true, // applies background color and foreground color for root element by default
         styled: true, // include daisyUI colors and design decisions for all components
