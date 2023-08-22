719f9e15c3fcfe36396d5c0343e6495b9d7b3e07
Jacky Fan <contact@jacky.fan>
2023-08-23 00:18:57 +0800

    ui: update hero and navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,13 @@
 export default function Navbar() {
     return (
-        <div className="navbar bg-base-200 fixed">
+        <div className="navbar bg-base-200 fixed z-50">
             <div className="flex-1">
                 <a className="btn btn-ghost normal-case text-xl"><span className="text-primary font-patrick_hand">Jacky FAN's</span> Website</a>
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>About</a></li>
-                    <li><a>Work</a></li>
+                    <li><a href="#!">About</a></li>
+                    <li><a href="#!">Work</a></li>
                     <li>
                         <details>
                             <summary>
--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,14 +1,23 @@
 export default function Hero() {
     return (
-        <section className="hero min-h-screen bg-base-200">
+        <section className="hero min-h-screen bg-base-200 relative">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src="/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+                {/* <img src="/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" /> */}
+                <div className="artboard artboard-horizontal phone-3">736×414</div>
                 <div>
                     <h1 className="text-5xl font-bold typed pr-2">Hello, I'm <span className="text-primary font-patrick_hand">Jacky FAN</span></h1>
                     <p className="py-6">I write code and eat computer bugs.</p>
-                    <button className="btn btn-primary">Checkout What I Did</button>
                 </div>
             </div>
+            <a href="#!" className="flex flex-col justify-center items-center absolute bottom-12 transition-all floating">
+                <p>About Me</p>
+                <span className="btn btn-circle btn-outline btn-sm border-0 mx-auto">
+                    <svg aria-hidden="true" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M15.75 17.25L12 21m0 0l-3.75-3.75M12 21V3" stroke-linecap="round" stroke-linejoin="round"></path>
+                    </svg>
+                </span>
+
+            </a>
         </section>
     );
 }
