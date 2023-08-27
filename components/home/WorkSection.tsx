2dfb728b4b958655bc5ac2d65288a4675a349dfb
Jacky Fan <contact@jacky.fan>
2023-08-27 23:59:30 +0800

    ui: add slogan to work section

--- components/home/WorkSection.tsx
+++ components/home/WorkSection.tsx
@@ -3,7 +3,11 @@ import PreviousWorkSlider from './PreviousWorksSlider';
 
 export default function Works() {
     return (
-        <section className=" min-h-screen max-w-screen bg-base-100 pt-10 relative">
+        <section className="min-h-screen max-w-screen bg-base-100 pt-10 relative">
+            <div className="container mx-auto my-20">
+                <h2 className="font-bold text-4xl mb-2">Check Out My <span className="text-primary">Previous Works</span></h2>
+                <p>I have done various projects for school and hobbies. Most of them are related to Web.</p>
+            </div>
             <PreviousWorkSlider />
         </section>
     );
