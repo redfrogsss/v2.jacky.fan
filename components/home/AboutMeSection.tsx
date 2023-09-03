0ccc3b0a0eb23d29c8e2f87a13312c355c13c0c3
Jacky Fan <contact@jacky.fan>
2023-09-03 20:46:38 +0800

    ui: update about me add tech stack

--- components/home/AboutMeSection.tsx
+++ components/home/AboutMeSection.tsx
@@ -19,7 +19,61 @@ export default function AboutMeSection() {
 
                 <p className="text-xl mb-8">I recently graduated with a <span className="text-primary">Bachelor Degree in Computing</span> from The Hong Kong Polytechnic University.</p>
 
-                <p className="text-xl mb-8">I am passionate about Programming, Web Technology and Problem Solving.</p>
+                {/* <p className="text-xl mb-8">I am passionate about Programming, Web Technology and Problem Solving.</p> */}
+
+                <p className="text-xl mb-8">I enjoy working on <span className="text-primary">Web Development</span> projects, especially with these technologies:</p>
+
+                <div className="rounded-md glass w-fit flex flex-row gap-4 mb-8 pb-2 px-4">
+                    <div className="w-30">
+                        <img src="/react.svg" alt="React" className="w-24 h-24" />
+                        <p className="text-center">
+                            ReactJS
+                        </p>
+                    </div>
+
+                    <div className="w-30">
+                        <img src="/typescript.svg" alt="Typescript" className="w-24 h-24" />
+                        <p className="text-center">
+                            TypeScript
+                        </p>
+                    </div>
+
+                    <div className="w-30">
+                        <img src="/html5.svg" alt="HTML5" className="w-24 h-24" />
+                        <p className="text-center">
+                            HTML5
+                        </p>
+                    </div>
+
+                    <div className="w-30">
+                        <img src="/css.svg" alt="CSS" className="w-24 h-24" />
+                        <p className="text-center">
+                            CSS
+                        </p>
+                    </div>
+
+                    <div className="w-30">
+                        <img src="/mysql.svg" alt="MySQL" className="w-24 h-24" />
+                        <p className="text-center">
+                            MySQL
+                        </p>
+                    </div>
+
+                    <div className="w-30">
+                        <img src="/git.svg" alt="Git" className="w-24 h-24" />
+                        <p className="text-center">
+                            Git
+                        </p>
+                    </div>
+
+                    <div className="w-30">
+                        <img src="/docker.svg" alt="Docker" className="w-24 h-24" />
+                        <p className="text-center">
+                            Docker
+                        </p>
+                    </div>
+
+                </div>
 
                 <button className="btn btn-neutral">
                     Check out my projects
