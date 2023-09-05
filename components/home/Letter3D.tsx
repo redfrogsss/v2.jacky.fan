93ddc784dbd0328c66eea7d25bd14d44afc45bed
Jacky Fan <contact@jacky.fan>
2023-09-05 22:34:48 +0800

    fix: 3d scene memory leak issue

--- components/home/Letter3D.tsx
+++ components/home/Letter3D.tsx
@@ -1,12 +1,36 @@
 
 'use client'
 
-import { Suspense } from "react"
-import { Canvas, useLoader } from "@react-three/fiber"
-import { Environment, OrbitControls } from "@react-three/drei"
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
+import { Canvas, useLoader } from "@react-three/fiber";
+import { Environment, OrbitControls } from "@react-three/drei";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Suspense, useRef, useState, useEffect } from "react";
 
 export default function Letter3D() {
+
+    const [inView, setInView] = useState(false);
+    const ref = useRef(null);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                setInView(entry.isIntersecting);
+                console.log("entry.isIntersecting", entry.isIntersecting)
+            },
+            {
+                rootMargin: "0px",
+                threshold: 0,
+            }
+        );
+        if (ref.current) {
+            observer.observe(ref.current);
+        }
+        return () => {
+            if (ref.current) {
+                observer.unobserve(ref.current);
+            }
+        };
+    }, []);
 
     function calcRotation(degrees: number): number {
         return (degrees * Math.PI) / 180;
@@ -24,16 +48,19 @@ export default function Letter3D() {
     };
 
     return (
-        <div className="grid place-items-center h-full w-1/3 absolute -top-0 -right-24">
-            <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 4], fov: 50 }}>
-                <ambientLight intensity={0.7} />
-                {/* <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow /> */}
-                <Suspense fallback={null}>
-                    <Model />
-                    <Environment preset="city" />
-                </Suspense>
-                {/* <OrbitControls autoRotate={false} /> */}
-            </Canvas>
+        <div ref={ref} className="grid place-items-center h-full w-1/3 absolute -top-0 -right-24">
+            {inView &&
+                <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 4], fov: 50 }}>
+                    <ambientLight intensity={0.7} />
+                    {/* <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow /> */}
+                    <Suspense fallback={null}>
+                        <Model />
+                        <Environment preset="city" />
+                    </Suspense>
+                    {/* <OrbitControls autoRotate={false} /> */}
+                </Canvas>
+            }
+
         </div>
     );
 }
--- components/home/Magnifier3D.tsx
+++ components/home/Magnifier3D.tsx
@@ -1,11 +1,34 @@
 'use client'
 
-import { Suspense } from "react"
+import { Suspense, useRef, useState, useEffect } from "react"
 import { Canvas, useLoader } from "@react-three/fiber"
 import { Environment, OrbitControls } from "@react-three/drei"
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 export default function Magnifier3D() {
+    const [inView, setInView] = useState(false);
+    const ref = useRef(null);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                setInView(entry.isIntersecting);
+                console.log("entry.isIntersecting", entry.isIntersecting)
+            },
+            {
+                rootMargin: "0px",
+                threshold: 0,
+            }
+        );
+        if (ref.current) {
+            observer.observe(ref.current);
+        }
+        return () => {
+            if (ref.current) {
+                observer.unobserve(ref.current);
+            }
+        };
+    }, []);
 
     function calcRotation(degrees: number): number {
         return (degrees * Math.PI) / 180;
@@ -23,16 +46,18 @@ export default function Magnifier3D() {
     };
 
     return (
-        <div className="grid place-items-center h-full w-2/3 absolute -top-16 -right-72">
-            <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 4], fov: 50 }}>
-                <ambientLight intensity={0.7} />
-                {/* <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow /> */}
-                <Suspense fallback={null}>
-                    <Model />
-                    <Environment preset="city" />
-                </Suspense>
-                {/* <OrbitControls autoRotate={false} /> */}
-            </Canvas>
+        <div ref={ref} className="grid place-items-center h-full w-2/3 absolute -top-16 -right-72">
+            {inView && (
+                <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 4], fov: 50 }}>
+                    <ambientLight intensity={0.7} />
+                    {/* <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow /> */}
+                    <Suspense fallback={null}>
+                        <Model />
+                        <Environment preset="city" />
+                    </Suspense>
+                    {/* <OrbitControls autoRotate={false} /> */}
+                </Canvas>
+            )}
         </div>
     );
 }
