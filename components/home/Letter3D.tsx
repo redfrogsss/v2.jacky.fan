5e5a83465a7c1879641a721b37a109d52db4fc0e
Jacky Fan <contact@jacky.fan>
2023-09-05 23:51:12 +0800

    feat: 3d objects scroll animation

--- components/home/Letter3D.tsx
+++ components/home/Letter3D.tsx
@@ -5,17 +5,26 @@ import { Canvas, useLoader } from "@react-three/fiber";
 import { Environment, OrbitControls } from "@react-three/drei";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { Suspense, useRef, useState, useEffect } from "react";
+import { motion, useMotionValueEvent, useScroll } from "framer-motion"
 
 export default function Letter3D() {
 
     const [inView, setInView] = useState(false);
     const ref = useRef(null);
+    const [modelRotation, setModelRotation] = useState([calcRotation(-30), calcRotation(120), calcRotation(0)]);
+    const { scrollYProgress } = useScroll();
+    const gltf = useLoader(GLTFLoader, "/letter/scene.gltf");
+
+    useMotionValueEvent(scrollYProgress, "change", (latest) => {
+        if (!inView) { return; }
+        setModelRotation([calcRotation(latest * 360 / 4 + 10), calcRotation(latest * 360 / 4 + 0), calcRotation(latest * 360 / 4 + 180)]);
+    })
 
     useEffect(() => {
         const observer = new IntersectionObserver(
             ([entry]) => {
                 setInView(entry.isIntersecting);
-                console.log("entry.isIntersecting", entry.isIntersecting)
+                // console.log("entry.isIntersecting", entry.isIntersecting)
             },
             {
                 rootMargin: "0px",
@@ -36,17 +45,6 @@ export default function Letter3D() {
         return (degrees * Math.PI) / 180;
     }
 
-    const Model = () => {
-        // location of the 3D model
-        const gltf = useLoader(GLTFLoader, "/letter/scene.gltf");
-        return (
-            <>
-                {/* Use scale to control the size of the 3D model */}
-                <primitive object={gltf.scene} scale={1.1} rotation={[calcRotation(-30), calcRotation(120), calcRotation(10)]} />
-            </>
-        );
-    };
-
     return (
         <div ref={ref} className="grid place-items-center h-full w-1/3 absolute -top-0 -right-24">
             {inView &&
@@ -54,7 +52,7 @@ export default function Letter3D() {
                     <ambientLight intensity={0.7} />
                     {/* <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow /> */}
                     <Suspense fallback={null}>
-                        <Model />
+                        <primitive object={gltf.scene} scale={1.1} rotation={modelRotation} />
                         <Environment preset="city" />
                     </Suspense>
                     {/* <OrbitControls autoRotate={false} /> */}
--- components/home/Magnifier3D.tsx
+++ components/home/Magnifier3D.tsx
@@ -4,20 +4,29 @@ import { Suspense, useRef, useState, useEffect } from "react"
 import { Canvas, useLoader } from "@react-three/fiber"
 import { Environment, OrbitControls } from "@react-three/drei"
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { motion, useMotionValueEvent, useScroll } from "framer-motion"
 
 export default function Magnifier3D() {
     const [inView, setInView] = useState(false);
     const ref = useRef(null);
+    const [modelRotation, setModelRotation] = useState([calcRotation(90), calcRotation(30), calcRotation(0)]);
+    const { scrollYProgress } = useScroll();
+    const gltf = useLoader(GLTFLoader, "/magnifier/scene.gltf");
+
+    useMotionValueEvent(scrollYProgress, "change", (latest) => {
+        if (!inView) { return; }
+        setModelRotation([calcRotation(90), calcRotation(latest * 360 / 4 + 10), calcRotation(latest * 360 / 4 - 30)]);
+    })
 
     useEffect(() => {
         const observer = new IntersectionObserver(
             ([entry]) => {
+                // console.log("entry.isIntersecting", entry.isIntersecting);
                 setInView(entry.isIntersecting);
-                console.log("entry.isIntersecting", entry.isIntersecting)
             },
             {
                 rootMargin: "0px",
-                threshold: 0,
+                threshold: [0, 1],
             }
         );
         if (ref.current) {
@@ -34,30 +43,23 @@ export default function Magnifier3D() {
         return (degrees * Math.PI) / 180;
     }
 
-    const Model = () => {
-        // location of the 3D model
-        const gltf = useLoader(GLTFLoader, "/magnifier/scene.gltf");
-        return (
-            <>
-                {/* Use scale to control the size of the 3D model */}
-                <primitive object={gltf.scene} scale={.0015} rotation={[calcRotation(90), calcRotation(30), calcRotation(0)]} />
-            </>
-        );
-    };
-
     return (
-        <div ref={ref} className="grid place-items-center h-full w-2/3 absolute -top-16 -right-72">
-            {inView && (
-                <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 4], fov: 50 }}>
-                    <ambientLight intensity={0.7} />
-                    {/* <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow /> */}
-                    <Suspense fallback={null}>
-                        <Model />
-                        <Environment preset="city" />
-                    </Suspense>
-                    {/* <OrbitControls autoRotate={false} /> */}
-                </Canvas>
-            )}
-        </div>
+        <>
+            <div ref={ref} className="grid place-items-center h-full w-2/3 absolute -top-16 -right-72">
+                {inView && (
+                    <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 4], fov: 50 }}>
+                        <ambientLight intensity={0.7} />
+                        {/* <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow /> */}
+                        <Suspense fallback={null}>
+                            {/* <Model /> */}
+                            <primitive object={gltf.scene} scale={.0015} rotation={modelRotation} />
+
+                            <Environment preset="city" />
+                        </Suspense>
+                        {/* <OrbitControls autoRotate={false} /> */}
+                    </Canvas>
+                )}
+            </div>
+        </>
     );
 }
