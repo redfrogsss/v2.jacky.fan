4bc1969e28ef944d2272fe32d04ed7c4c9432d61
Jacky Fan <contact@jacky.fan>
2023-09-13 23:01:57 +0800

    fix: object undefined when load

--- components/home/Letter3D.tsx
+++ components/home/Letter3D.tsx
@@ -53,7 +53,7 @@ export default function Letter3D() {
                     <ambientLight intensity={0.7} />
                     {/* <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow /> */}
                     <Suspense fallback={null}>
-                        <primitive object={gltf} scale={1.1} rotation={modelRotation} />
+                        {gltf && <primitive object={gltf} scale={1.1} rotation={modelRotation} />}
                         <Environment preset="city" />
                     </Suspense>
                     {/* <OrbitControls autoRotate={false} /> */}
--- components/home/Magnifier3D.tsx
+++ components/home/Magnifier3D.tsx
@@ -57,7 +57,7 @@ export default function Magnifier3D() {
                         {/* <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow /> */}
                         <Suspense fallback={null}>
                             {/* <Model /> */}
-                            <primitive object={gltf} scale={.0013} rotation={modelRotation} />
+                            {gltf && <primitive object={gltf} scale={.0013} rotation={modelRotation} />}
 
                             <Environment preset="city" />
                         </Suspense>
