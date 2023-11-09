c0e7d6b2913c8f8a0ccafb9504da16624986df93
Jacky Fan <contact@jacky.fan>
2023-11-09 23:57:00 +0800

    ui: add fadeinbottom to work and contact section

--- components/home/ContactMeSection.tsx
+++ components/home/ContactMeSection.tsx
@@ -1,26 +1,32 @@
 import Letter3D from './Letter3D';
 import ContactForm from "./ContactForm";
+import FadeInBottom from '../animation/FadeInBottom';
 
 export default function ContactMeSection() {
     return (
         <section className="min-h-[900px] bg-base-100 relative overflow-x-clip">
             <div className="container mx-auto py-12 md:py-32">
-                <h2 className="font-bold text-3xl md:text-5xl mb-8 md:mb-16">
-                    <span className="text-sm md:text-lg flex font-light drop-shadow-sm">
-                        <hr className="w-8 md:w-16 inline-block my-auto mr-2 border-base-content" />
-                        Feel Free to
-                    </span>
-                    <span className="text-primary drop-shadow">Contact</span> Me
-                </h2>
+                <FadeInBottom>
+                    <h2 className="font-bold text-3xl md:text-5xl mb-8 md:mb-16">
+                        <span className="text-sm md:text-lg flex font-light drop-shadow-sm">
+                            <hr className="w-8 md:w-16 inline-block my-auto mr-2 border-base-content" />
+                            Feel Free to
+                        </span>
+                        <span className="text-primary drop-shadow">Contact</span> Me
+                    </h2>
+                </FadeInBottom>
 
-                <p className="text-md md:text-xl mb-4 md:mb-8">Feel free to leave me a message and say Hello to me.</p>
+                <FadeInBottom>
+                    <p className="text-md md:text-xl mb-4 md:mb-8">Feel free to leave me a message and say Hello to me.</p>
+                </FadeInBottom>
 
-                <div className="card w-full xl:w-2/3 bg-base-300 shadow-xl">
-                    <div className="card-body">
-                        <ContactForm />
+                <FadeInBottom>
+                    <div className="card w-full xl:w-2/3 bg-base-300 shadow-xl">
+                        <div className="card-body">
+                            <ContactForm />
+                        </div>
                     </div>
-                </div>
-
+                </FadeInBottom>
             </div>
 
             <Letter3D />
--- components/home/WorkSection.tsx
+++ components/home/WorkSection.tsx
@@ -1,22 +1,31 @@
 import 'swiper/css';
 import PreviousWorkSlider from './PreviousWorksSlider';
 import { CursorArrowRaysIcon } from "@heroicons/react/24/outline";
+import FadeInBottom from '../animation/FadeInBottom';
 
 export default function WorkSection() {
     return (
         <section id="work" className="min-h-[980px] bg-base-100 pt-10 relative">
             <div className="container mx-auto pt-12">
-                <h2 className="font-bold text-3xl md:text-5xl mb-8 md:mb-16 drop-shadow">
-                    <span className="text-sm md:text-lg flex font-light drop-shadow-sm">
-                        <hr className="w-8 md:w-16 inline-block my-auto mr-2 border-base-content" />
-                        Checkout
-                    </span>
-                    My <span className="text-primary">Previous Projects</span>
-                    <CursorArrowRaysIcon className="w-6 h-6 md:h-10 md:w-10 text-base-content inline ml-2" />
-                </h2>
-                <p className="text-md md:text-xl mb-4 md:mb-8">I have worked on different projects for school and in my free time, and most of them have involved things related to the web.</p>
+                <FadeInBottom>
+                    <h2 className="font-bold text-3xl md:text-5xl mb-8 md:mb-16 drop-shadow">
+                        <span className="text-sm md:text-lg flex font-light drop-shadow-sm">
+                            <hr className="w-8 md:w-16 inline-block my-auto mr-2 border-base-content" />
+                            Checkout
+                        </span>
+                        My <span className="text-primary">Previous Projects</span>
+                        <CursorArrowRaysIcon className="w-6 h-6 md:h-10 md:w-10 text-base-content inline ml-2" />
+                    </h2>
+                </FadeInBottom>
+
+                <FadeInBottom>
+                    <p className="text-md md:text-xl mb-4 md:mb-8">I have worked on different projects for school and in my free time, and most of them have involved things related to the web.</p>
+                </FadeInBottom>
             </div>
-            <PreviousWorkSlider />
+
+            <FadeInBottom>
+                <PreviousWorkSlider />
+            </FadeInBottom>
         </section>
     );
 }
