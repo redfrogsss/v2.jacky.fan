babf4ecf68bb25bce42f6b68467375e8fb3a4d52
Jacky Fan <contact@jacky.fan>
2023-11-13 22:19:23 +0800

    chore: move context provider into layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { outfit, patrick_hand } from './fonts'
 
+import { AlertContextProvider } from '@/contexts/AlertContext'
+import { RecaptchaProviders } from '@/providers/RecaptchaProvider'
+
 export const metadata: Metadata = {
   title: "Jacky FAN",
   description: 'Hello I am Jacky FAN, a front end developer in Hong Kong who focus on building high-quality websites.',
@@ -15,7 +18,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${outfit.className} ${outfit.variable} ${patrick_hand.variable} overflow-x-hidden`}>
-        {children}
+        <RecaptchaProviders>
+          <AlertContextProvider>
+            {children}
+          </AlertContextProvider >
+        </RecaptchaProviders>
       </body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -7,26 +7,20 @@ import ContactMeSection from '@/components/home/ContactMeSection'
 import Footer from '@/components/home/Footer'
 import Hero from '@/components/home/Hero'
 import WorkSection from '@/components/home/WorkSection'
-import { AlertContextProvider } from '@/contexts/AlertContext'
-import { RecaptchaProviders } from '@/providers/RecaptchaProvider'
 
 export default function Home() {
   return (
     <main className="relative">
-      <RecaptchaProviders>
-        <AlertContextProvider>
-          <LoadingScreen>
-            <Navbar />
-            <Hero />
-            <AboutMeSection />
-            <WorkSection />
-            <ContactMeSection />
-            <BottomSection />
-            <Footer />
-            <Alert />
-          </LoadingScreen>
-        </AlertContextProvider >
-      </RecaptchaProviders>
+      <LoadingScreen>
+        <Navbar />
+        <Hero />
+        <AboutMeSection />
+        <WorkSection />
+        <ContactMeSection />
+        <BottomSection />
+        <Footer />
+        <Alert />
+      </LoadingScreen>
     </main >
   )
 }
