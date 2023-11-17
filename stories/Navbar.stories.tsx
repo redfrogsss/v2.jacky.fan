ce44c4bf09941ba8e2ce7da07a83d81eccd96162
Jacky Fan <contact@jacky.fan>
2023-11-18 00:01:48 +0800

    util: fix storyboard absolute path and import tailwindcss

---
+++ stories/Navbar.stories.tsx
@@ -0,0 +1,19 @@
+// YourComponent.stories.ts|tsx
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Navbar from '@/components/Navbar';
+
+//👇 This default export determines where your story goes in the story list
+const meta: Meta<typeof Navbar> = {
+    component: Navbar,
+};
+
+export default meta;
+type Story = StoryObj<typeof Navbar>;
+
+export const FirstStory: Story = {
+    args: {
+        //👇 The args you need here will depend on your component
+    },
+};
