449020ad3d92b96e8956daee10194ccbf01c2065
Jacky Fan <contact@jacky.fan>
2023-11-19 13:54:41 +0000

    feat: add basic loading screen logic with NextJS Suspense feature

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,20 @@
-import { Alert, Footer, Navbar } from '@/components'
+import { Suspense } from 'react'
+import { Alert, Footer, LoadingScreen, Navbar } from '@/components'
 import { AboutMeSection, ContactMeSection, HeroSection, BottomSection, WorkSection } from '@/components/home/sections'
 
 export default function Home() {
   return (
     <main className="relative">
-      <Navbar />
-      <HeroSection />
-      <AboutMeSection />
-      <WorkSection />
-      <ContactMeSection />
-      <BottomSection />
-      <Footer />
-      <Alert />
+      <Suspense fallback={<LoadingScreen />}>
+        <Navbar />
+        <HeroSection />
+        <AboutMeSection />
+        <WorkSection />
+        <ContactMeSection />
+        <BottomSection />
+        <Footer />
+        <Alert />
+      </Suspense>
     </main >
   )
 }
--- components/LoadingScreen.tsx
+++ components/LoadingScreen.tsx
@@ -1,70 +1,19 @@
-'use client'
-
 import React from "react"
-import { motion } from "framer-motion";
 import Pien from "./icons/Pien";
 
-export default function LoadingScreen({ children }: { children: React.ReactNode }) {
+export default function LoadingScreen() {
     return (
         <>
-
-            <motion.div className="bg-base-300 absolute h-screen w-screen top-0 left-0 z-10">
-                <motion.div
-                    className="absolute inset-0 bg-base-100 z-20"
-                    animate={{
-                        inset: ["0px", "0% 0% 100% 0%"],
-                        display: ["block"],
-                        opacity: ["100%", "0%"],
-                        // zIndex: [20, 0],
-                        transitionEnd: {
-                            // opacity: 0,
-                            display: "none",
-                        },
-                    }}
-                    transition={{
-                        delay: 1.5 * 3,
-                        duration: 1.5,
-                        ease: "easeInOut",
-                    }}
-                >
+            <div className="bg-base-300 absolute h-screen w-screen top-0 left-0 z-10">
+                <div >
                     <div className="flex flex-col items-center justify-center h-screen w-100">
-                        <motion.div
-                            className="w-16 aspect-square text-base-content"
-                            animate={{
-                                rotate: 360,
-                                y: [0, -10, 0],
-                            }}
-                            exit={{
-                                opacity: 0,
-                            }}
-                            transition={{
-                                duration: 1.5,
-                                repeat: 4,
-                                ease: "easeInOut",
-                            }}
-                        >
+                        <div >
                             <Pien color="currentColor" className="w-full h-full" />
-                        </motion.div>
+                        </div>
                         <p className="mt-2">Loading...</p>
                     </div>
-                </motion.div >
-            </motion.div>
-            <motion.div
-                className=""
-                initial={{
-                    display: "none",
-                }}
-                animate={{
-                    display: "block",
-                }}
-                transition={{
-                    delay: 1.5 * 3,
-                    duration: 1.5,
-                    ease: "easeInOut",
-                }}
-            >
-                {children}
-            </motion.div>
+                </div >
+            </div>
         </>
     )
 }
