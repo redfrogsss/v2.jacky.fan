2a5873a73bca394ffd3060148357713901813f76
Jacky Fan <contact@jacky.fan>
2023-11-20 15:27:52 +0000

    ui: update floating arrow remove text

--- components/home/FloatingArrow.tsx
+++ components/home/FloatingArrow.tsx
@@ -11,7 +11,7 @@ export default function FloatingArrow(props: FloatingArrowProps) {
             href="#about"
             className="flex flex-col justify-center items-center absolute bottom-[10%] transition-all w-fit z-20 left-0 right-0 mx-auto floating"
         >
-            <p className="drop-shadow-sm">{props.text}</p>
+            {/* <p className="drop-shadow-sm">{props.text}</p> */}
             <span className="btn btn-circle btn-outline btn-sm border-0 mx-auto">
                 <ArrowLongDownIcon className="h-6 w-6 text-base-content hover:text-base-300" />
             </span>
