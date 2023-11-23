faebd47fb207dd7d31330ad810aa0c6a28a2be36
Jacky Fan <contact@jacky.fan>
2023-11-24 00:05:07 +0800

    fix: recaptcha failed to work

--- app/api/captcha/route.ts
+++ app/api/captcha/route.ts
@@ -1,16 +1,25 @@
-import axios from "axios";
-
 export const runtime = "edge"; // cloudflare edge
 
 export async function POST(request: Request) {
     const { token } = await request.json();
     try {
         if (!token) throw new Error();
-        const res = await axios.post(
-            `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${token}`
+
+        const res = await fetch(
+            `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${token}`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+            }
         );
 
-        if (res.data.score > 0.5) {
+        if (!res.ok) throw new Error(res.statusText);
+
+        const data = await res.json();
+
+        if (data.score > 0.5) {
             return new Response(null, {
                 status: 200,
             });
@@ -22,6 +31,7 @@ export async function POST(request: Request) {
     } catch (error) {
         return new Response(null, {
             status: 400,
+            statusText: JSON.stringify(error),
         });
     }
 }
