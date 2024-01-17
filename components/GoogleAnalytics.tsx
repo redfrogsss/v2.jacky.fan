485d100252a8f0ec0f2a6de079f4cbfd4f94a27d
Jacky Fan <contact@jacky.fan>
2024-01-17 23:15:57 +0800

    Fix: google analytic add next/script id attribute

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -4,7 +4,7 @@ export default function GoogleAnalytics() {
     return (
         <>
             <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_ID}`}></Script>
-            <Script>
+            <Script id="google-analytics">
                 {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
