ab82bef164319c6eaa5d92319ea564eab1cd6c9e
Jacky Fan <contact@jacky.fan>
2024-04-04 15:42:33 +0800

    feat: fetch project details from md file

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,19 +1,59 @@
+'use client'
+
 import FadeInBottom from "@/components/animation/FadeInBottom";
 import { ActiveLink, Page, SectionContainer } from "@/components/basic";
 import { Heading } from "@/components/visual";
 import Image from "next/image";
 import { ArrowLeftIcon } from "@heroicons/react/24/outline";
+import { projectInfos } from "@/hooks/useProjectInfo";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import Tabs from "@/components/Tabs";
+import { LinkIcon } from "@heroicons/react/24/outline";
 
 export default function ProjectDescPage({ params }: { params: { slug: string } }) {
 
     const projectPageNmae = params.slug;
+    const [projectData, setProjectData] = useState<any | undefined>(undefined);
+    const [docs, setDocs] = useState<any[]>([]);
+
+    const updateDocs = async (docs : any) => {
+        if (!docs) return;
+
+        let newDocsState = [];
+
+        for (let i = 0; i < docs.length; i++) {
+            try {
+                let md = await fetch(docs[i].link);
+                let mdText = await md.text();
+                
+                newDocsState.push({...docs[i], md: mdText});
+            } catch (error) {
+                console.error("md not found");
+            }
+        }
+        
+        setDocs(newDocsState);
+    }
+
+    useEffect(()=>{
+        setProjectData(projectInfos.filter(proj => proj.proj == projectPageNmae)[0] ?? undefined);
+    }, [])
+
+    useEffect(()=> {
+        updateDocs(projectData?.docs);
+    }, [projectData])
+
+    // debug 
+    // useEffect(()=>{console.log(projectData)}, [projectData])
+    // useEffect(()=>{console.log(docs)}, [docs])
 
     return (
         <Page>
             <SectionContainer extendRightSpacing={true} extraClassName="bg-base-300 -mt-1">
                 <FadeInBottom>
                     <div className="mb-4 md:mb-8">
-                        <ActiveLink href="javascript:history.back()" className="btn btn-outline w-fit">
+                        <ActiveLink href="/projects" className="btn btn-outline w-fit">
                             <ArrowLeftIcon className="h-[1em]" />
                             Back
                         </ActiveLink>
@@ -21,40 +61,34 @@ export default function ProjectDescPage({ params }: { params: { slug: string } }
 
                     <div className="flex flex-col lg:flex-row gap-x-4 gap-y-8">
                         <div className="w-full lg:w-2/3">
-                            <Heading topTitle="Side Project" leftTitle="v2.Jacky.Fan" />
+                            <Heading topTitle="My Project" leftTitle={projectData?.name} />
                             <div className="flex flex-row flex-wrap gap-2 mb-4 lg:mb-8">
-                                <div className="badge badge-lg badge-primary badge-outline">primary</div>
-                                <div className="badge badge-lg badge-primary badge-outline">primary</div>
-                                <div className="badge badge-lg badge-primary badge-outline">primary</div>
-                                <div className="badge badge-lg badge-primary badge-outline">primary</div>
-                                <div className="badge badge-lg badge-primary badge-outline">primary</div>
-                                <div className="badge badge-lg badge-primary badge-outline">primary</div>
-                                <div className="badge badge-lg badge-primary badge-outline">primary</div>
+                                {projectData?.tags.map((tag, i) => <div className="badge badge-lg badge-primary badge-outline" key={i}>{tag}</div>)}
+                                
                             </div>
                             <p className="mb-4 lg:mb-8">
-                                v2.jacky.fan is a personal website built with Next.js, Tailwind CSS, and Framer Motion.
-                                v2.jacky.fan is a personal website built with Next.js, Tailwind CSS, and Framer Motion.
-                                v2.jacky.fan is a personal website built with Next.js, Tailwind CSS, and Framer Motion.
+                                {projectData?.desc}
                             </p>
                             <div className="flex flex-row flex-wrap gap-2">
-                                <button className="btn btn-primary">GitHub</button>
-                                <button className="btn btn-secondary">Secondary</button>
-                                <button className="btn btn-outline">Default</button>
+                                { projectData?.links.map((link, i) => <Link href={link.link} target="_blank" className={`btn ${i == 0 ? "btn-primary" : i == 1 ? "btn-primary" : "btn-outline" }`} key={i}>
+                                    <LinkIcon className="h-[1em]" />
+                                    {link.name}
+                                </Link>) }
                             </div>
-
 
                         </div>
                         <div className="w-full lg:w-1/2">
-                            <div className="mockup-window border bg-base-300 my-auto shadow-xl">
-                                <div className="flex justify-center px-4 py-16 bg-base-200">
-                                    {/* <figure> */}
-                                    {/* <Image
-                                        src={img}
-                                        fill={true}
+                            <div className="mockup-window border bg-base-300 my-auto shadow-xl transition-all proj-window-3d">
+                                <div className="flex justify-center px-4 py-4 bg-base-200">
+                                    <figure>
+                                    <Image
+                                        src={projectData?.img}
+                                        width={500}
+                                        height={0}
                                         alt="Project Name"
                                         className="object-contain"
-                                    /> */}
-                                    {/* </figure> */}
+                                    />
+                                    </figure>
                                 </div>
                             </div>
                         </div>
@@ -62,32 +96,8 @@ export default function ProjectDescPage({ params }: { params: { slug: string } }
                 </FadeInBottom>
             </SectionContainer>
 
-            <SectionContainer extendRightSpacing={true} topSpacing={true} bottomSpacing={false}>
-                <FadeInBottom extraClassName="animation-delay-500">
-                    <div role="tablist" className="tabs tabs-boxed w-fit mx-auto mb-8 md:mb-16">
-                        <a role="tab" className="tab tab-active font-black">Overview</a>
-                        <a role="tab" className="tab">Tech Spec</a>
-                        <a role="tab" className="tab">Screenshots</a>
-                    </div>
-                </FadeInBottom>
-            </SectionContainer>
+            <Tabs data={docs} />
 
-            <SectionContainer topSpacing={false} extendRightSpacing={false}>
-                <FadeInBottom extraClassName="animation-delay-500">
-                    <div id="overview" className="">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam blanditiis esse recusandae, nesciunt omnis tempora obcaecati reprehenderit mollitia in ratione dolores, nemo harum. Culpa veniam cumque obcaecati, accusamus dolorem quia.
-                            Nulla reprehenderit nesciunt, itaque cum magnam mollitia repellat exercitationem. Quisquam natus ad quas dolorem, ea deserunt omnis repellendus soluta rem, optio maxime eius fuga culpa! Consequatur laboriosam deleniti distinctio provident!
-                            Sed, explicabo magni! Placeat perferendis veritatis eaque sapiente autem maxime quaerat. Rem pariatur similique sed distinctio possimus officia maxime consequuntur laborum. Esse atque sapiente corporis officiis culpa aliquam numquam quis.
-                            Dolorem aspernatur quo dicta vero totam praesentium reprehenderit minus tenetur voluptates perspiciatis alias laboriosam, iste eveniet, sapiente quos, fugit eligendi temporibus facilis aliquam? Quasi totam adipisci libero illum excepturi natus.
-                            Magni necessitatibus aliquid ipsum neque dolores aliquam eius quidem quam nesciunt nihil architecto porro, sapiente, ut laudantium fuga totam at illum rerum. Facilis asperiores voluptatibus optio, cumque quos quaerat eum!
-                            Porro earum atque delectus aliquid repudiandae quisquam, ratione ducimus cum necessitatibus cupiditate ipsa obcaecati, illo adipisci excepturi vitae quam facere incidunt. Nostrum maxime maiores temporibus eaque modi veniam aliquid molestias?
-                            Quisquam sed voluptatibus sint dolore culpa sunt maiores doloremque? Minima dolorum, quas dolorem ullam, nostrum eaque sint reprehenderit, neque facere suscipit culpa exercitationem fugit. Cupiditate temporibus fugit nisi nostrum officiis!
-                            Beatae rem, vel similique quia, mollitia exercitationem amet sint repudiandae dolorum fuga eaque molestiae fugiat eius numquam quae ducimus, minus natus deserunt consectetur voluptates? Ratione dolores placeat nesciunt modi molestias.
-                            Labore doloribus, impedit aspernatur enim praesentium recusandae dolores! Laborum adipisci mollitia quis nulla veniam officia sint quaerat tempore, reiciendis iste quisquam ab obcaecati suscipit? Itaque, hic qui! Sunt, enim libero.
-                            Sapiente repellat ipsum necessitatibus praesentium officiis id laudantium debitis repudiandae unde odit molestiae laborum delectus earum eos, maxime nemo suscipit velit nostrum quae tenetur, quod doloremque assumenda eligendi illo? Unde.</p>
-                    </div>
-                </FadeInBottom>
-            </SectionContainer>
         </Page>
     );
 } 
---
+++ components/Tabs.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import FadeInBottom from "@/components/animation/FadeInBottom";
+import { ActiveLink, SectionContainer } from "@/components/basic";
+import { MouseEventHandler, useState } from "react";
+import Markdown from "react-markdown";
+
+export default function Tabs({data} : {data: any[]}) {
+
+    const [activeTab, setActiveTab] = useState<number>(0);
+
+    const selectTab = (e: React.MouseEvent<Element, MouseEvent>, i: number) => {
+        e.preventDefault();
+        setActiveTab(i);
+    }
+
+    return (
+        <>
+            <SectionContainer extendRightSpacing={true} topSpacing={true} bottomSpacing={false}>
+                <FadeInBottom extraClassName="animation-delay-500">
+                    <div role="tablist" className="tabs tabs-boxed w-fit mx-auto mb-8 md:mb-16">
+                        {data.map((item, i) =>
+                            <a role="tab" className={`tab${i == activeTab ? " tab-active font-black" : ""}`} key={i} onClick={(e) => selectTab(e, i)}>{item.name ?? ""}</a>
+                        )}
+                    </div>
+                </FadeInBottom>
+            </SectionContainer>
+
+            <SectionContainer topSpacing={false} extendRightSpacing={false}>
+                <FadeInBottom extraClassName="animation-delay-500">
+                    {data.map((item, i) =>
+                        <article id={item.name?.toString().toLowerCase().replace(" ", "-")} className={`prose text-md md:text-xl leading-6 md:leading-8 text-base-content${activeTab == i ? "" : " hidden"}`} key={i}>
+                            <Markdown>
+                                {item.md}
+                            </Markdown>
+                        </article>
+                    )}
+                </FadeInBottom>
+            </SectionContainer>
+        </>
+    );
+}
