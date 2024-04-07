8b0edd575a49571e9611330b9bacc9b219eb9225
Jacky Fan <contact@jacky.fan>
2024-04-07 22:27:55 +0800

    fix: banner component performance issue

--- components/home/projects/ProjectBanner.tsx
+++ components/home/projects/ProjectBanner.tsx
@@ -26,8 +26,10 @@ export default function ProjectBanner({
     const { scrollPos } = useContext(LocomotiveScrollPositionContext);
     const imgRef = useRef<HTMLDivElement | null>(null);
     const imgWrapperRef = useRef<HTMLDivElement | null>(null);
+    const componentRef = useRef<HTMLElement | null>(null);
     const [posPercentage, setPosPercentage] = useState<number>(100);
     const [showImg, setShowImg] = useState<boolean>(true);
+    const [inView, setInView] = useState(true);
 
     const speed = 50;
     const offset = "-250px";
@@ -35,10 +37,14 @@ export default function ProjectBanner({
     const { breakpoint } = useBreakPoint();
 
     useEffect(() => {
-        // setPosPercentage(clamp(((imgWrapperRef.current?.getBoundingClientRect().top ?? 100) / (window.innerHeight * 0.25)) * 100, -200, 0));
-        setPosPercentage(((imgWrapperRef.current?.getBoundingClientRect().top ?? 100) / (window.innerHeight * 0.25)) * 100);
+        if (!inView) return;
         const imgTop = imgRef.current?.getBoundingClientRect().top ?? 0;
-        setShowImg(imgTop < -200);
+
+        let newPosPercentage = ((imgTop ?? 100) / (window.innerHeight * 0.25)) * 100;
+        let newIsShowImg = imgTop < -200;
+
+        if (posPercentage != newPosPercentage) setPosPercentage(newPosPercentage);
+        if (newIsShowImg != showImg) setShowImg(newIsShowImg);
     }, [scrollPos]);
 
     // debug
@@ -51,10 +57,25 @@ export default function ProjectBanner({
         if (breakpoint == "sm" || breakpoint == "md") return true;
         return false;
     }
+    
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                setInView(entry.isIntersecting);
+            },
+            {
+                rootMargin: "0px",
+                threshold: [0, 1],
+            }
+        );
+        if (componentRef.current) {
+            observer.observe(componentRef.current);
+        }
+    }, []);
 
     return (
         <FadeInBottom>
-            <section className={`w-screen ${posReverse? "bg-gradient-to-r" : "bg-gradient-to-l"} from-accent to-primary text-primary-content mb-[80px] lg:mb-[350px] min-h-[38vh]`}>
+            <section className={`w-screen ${posReverse? "bg-gradient-to-r" : "bg-gradient-to-l"} from-accent to-primary text-primary-content mb-[80px] lg:mb-[350px] min-h-[38vh]`} ref={componentRef}>
                 <SectionContainer extendRightSpacing={true}>
                     <div className={`flex flex-col-reverse ${posReverse ? "lg:flex-row-reverse" : "lg:flex-row"} gap-x-16`}>
                         <div className="flex flex-col gap-y-4 w-full">
@@ -72,7 +93,7 @@ export default function ProjectBanner({
                         </div>
                         <div className="relative w-full" ref={imgWrapperRef}>
                             {(!posReverse) &&
-                                <div className={`relative lg:absolute left-0 w-full lg:w-[calc(50vw-2rem)] max-w-[924px] aspect-video lg:aspect-auto lg:pb-[100%] !sm:max-lg:top-0 transition-opacity ${showImg ? "opacity-50" : "opacity-100"}`} ref={imgRef} style={{ top: isSmallScreen() ? "-32px" : `calc(${speed}px * (${posPercentage} / 100) + ${offset})` }}>
+                                <div className={`relative lg:absolute left-0 w-full lg:w-[calc(50vw-2rem)] max-w-[924px] aspect-video lg:aspect-auto lg:pb-[100%] !sm:max-lg:top-0 transition-opacity ${showImg ? "opacity-50" : "opacity-100"}`} ref={imgRef} style={{ transform: `translateY(${isSmallScreen() ? "-32px" : `calc(${speed}px * (${posPercentage} / 100) + ${offset})`})` }}>
                                     <Image
                                         src={img}
                                         fill={true}
@@ -82,7 +103,7 @@ export default function ProjectBanner({
                                 </div>
                             }
                             {posReverse && 
-                                <div className={`relative lg:absolute right-0 w-full lg:w-[calc(50vw-2rem)] max-w-[924px] aspect-video lg:aspect-auto lg:pb-[100%] !sm:max-lg:top-0 transition-opacity ${showImg ? "opacity-50" : "opacity-100"}`} ref={imgRef} style={{ top: isSmallScreen() ? "-32px" : `calc(${speed}px * (${posPercentage} / 100) + ${offset})` }}>
+                                <div className={`relative lg:absolute right-0 w-full lg:w-[calc(50vw-2rem)] max-w-[924px] aspect-video lg:aspect-auto lg:pb-[100%] !sm:max-lg:top-0 transition-opacity ${showImg ? "opacity-50" : "opacity-100"}`} ref={imgRef} style={{ transform: `translateY(${isSmallScreen() ? "-32px" : `calc(${speed}px * (${posPercentage} / 100) + ${offset})`})` }}>
                                     <Image
                                         src={img}
                                         fill={true}
