96d6b5750f9da4c501da525862dcae54ea9b176e
Jacky Fan <contact@jacky.fan>
2024-06-23 03:06:01 +0800

    fix: build error

--- components/visual/bgHeading.tsx
+++ components/visual/bgHeading.tsx
@@ -4,7 +4,7 @@ export default function BgHeading({ title = "" }: { title?: string }) {
 
     return (
         <div className="hidden xl:flex absolute inset-0 -z-10 items-center justify-end w-screen h-screen opacity-10 pointer-events-none pr-8">
-            <span className="text-7xl 2xl:text-8xl text-primary block w-fit h-fit font-bold text-outlined text-right">{displayTitle.map((word, i) => <span className="block">{word}</span>)}</span>
+            <span className="text-7xl 2xl:text-8xl text-primary block w-fit h-fit font-bold text-outlined text-right">{displayTitle.map((word, i) => <span className="block" key={i}>{word}</span>)}</span>
         </div>
     );
 }
